be8f7bcc1a23004148ba7d3ce878d8c6c1873c5b
isahmet089 <isahmet089@gmail.com>
2025-02-20 22:48:30 +0300

    mango db ve .env

--- src/server.js
+++ src/server.js
@@ -1,7 +1,9 @@
 const app = require('./app');
-const PORT = process.env.PORT || 3000;
+require("dotenv").config();
+const connectDB=require("./config/dbConfig");
+const PORT = process.env.PORT;
 
-
+connectDB();
 app.listen(PORT, () => {
     console.log(`Sunucu ${PORT} portunda çalışıyor!`);
 }); 
